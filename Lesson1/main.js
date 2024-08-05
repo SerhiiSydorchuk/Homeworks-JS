208f63841f904cd86393d00e7158b3e939e0ef52
SerhiiSydorchuk <sydorchuks93@gmail.com>
2024-08-05 21:51:38 +0200

    lesson1

---
+++ Lesson1/main.js
@@ -0,0 +1,45 @@
+let word1 = 'hello';
+console.log (word1);
+let word2 = 'owu';
+console.log(word2);
+let word3 = 'com';
+console.log(word3);
+let word4 = 'ua';
+console.log(word4);
+let num1 = 1;
+console.log(num1);
+let num2 =10;
+console.log(num2);
+let num3 = -999;
+console.log(num3);
+let num4 = 123;
+console.log(num4);
+const num5 = 3.14;
+console.log(num5);
+let num6 = 2.7;
+console.log(num6);
+let num7 = 16 ;
+console.log(num7);
+let bool1 = false;
+console.log(bool1);
+let bool2 = true;
+console.log(bool2);
+
+let firstName = 'Сергій';
+let secondName ='Сидорчук'
+let middleName=' Миколайович';
+let name = secondName + ' '+ firstName +' '+ middleName;
+console.log(name);
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+
+let prom1 = prompt('Enter First Name');
+console.log(prom1);
+let prom2 = prompt('Enter Second Name');
+console.log(prom2);
+let prom3 = prompt('Enter Age');
+console.log(+prom3);
