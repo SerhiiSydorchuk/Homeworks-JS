422328e4f525a6aaed0feef01e9aadeb7897eb4b
SerhiiSydorchuk <sydorchuks93@gmail.com>
2024-08-21 03:17:36 +0200

    add .toSring()

--- lesson6/main.js
+++ lesson6/main.js
@@ -36,7 +36,7 @@ console.log(str1.split(' '));
 // - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
 
 const arrNum = [10, 8, -7, 55, 987, -1011, 0, 1050, 0];
-const arrNumSrt = arrNum.map(number=> number+'');
+const arrNumSrt = arrNum.map(number=> number.toString());
 console.log(arrNumSrt);
 
 // #5hqyKTfmc
